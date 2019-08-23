74f56db4ebfd7ce070e09e3c27939bba40dcba83
masfelix <masfelix@gmail.com>
2019-08-23 16:51:51 -0300

    segundo desafio do bootcamp

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '123Mudar',
+  database: 'meetupclone',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
